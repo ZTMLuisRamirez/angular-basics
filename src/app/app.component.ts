360fab24c3a2dd6cc10cf45b192b80085d9794bf
Luis R <jaskokoyn@gmail.com>
2024-06-24 14:35:25 -0400

    Section 2: Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  // template: `<p>Hello world!</p>`,
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  // styles: ['']
 })
-export class AppComponent {
-  title = 'angular-basics';
-}
+export class AppComponent {}
